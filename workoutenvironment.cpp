954ef949a2b13109e7ec1bbce1b0445ee0a596b8
Richard Guerre <guerre.richard@me.com>
2019-09-23 17:10:47 +0800

    finished workoutenvironment.cpp implementation

--- workoutenvironment.cpp
+++ workoutenvironment.cpp
@@ -33,7 +33,7 @@ int WorkoutEnvironment::get_entry_fee() const{
 bool WorkoutEnvironment::add_workout(const Workout& workout, int add_index){
   if(add_index >= 0
     && add_index <= current_num_of_workouts
-    && current_num_of_workouts <= MAX_NUM_OF_WORKOUTS){
+    && current_num_of_workouts < MAX_NUM_OF_WORKOUTS){
       available_workouts[add_index] = workout;
       current_num_of_workouts++;
       return true;
@@ -43,7 +43,7 @@ bool WorkoutEnvironment::add_workout(const Workout& workout, int add_index){
 bool WorkoutEnvironment::remove_workout(int remove_index){
   if(remove_index >= 0
     && remove_index <= current_num_of_workouts
-    && current_num_of_workouts >= 0){
+    && current_num_of_workouts > 0){
       for(int i=remove_index; i<current_num_of_workouts; i++)
         available_workouts[i] = available_workouts[i+1];
       return true;
@@ -51,11 +51,38 @@ bool WorkoutEnvironment::remove_workout(int remove_index){
   return false;
 }
 int WorkoutEnvironment::participant_index(const Buddy* buddy) const{
-  return 0;
+  for(int i=0; i<current_num_of_participants; i++){
+    if(participants[i] == buddy){
+      return i;
+    }
+  }
+  return -1;
 }
 bool WorkoutEnvironment::register_participant(Buddy* buddy){
+  if(participant_index(buddy) != -1 && entry_fee <= buddy->get_money()){
+    buddy->set_money(buddy->get_money() - entry_fee);
+    {
+      Buddy **array = new Buddy*[current_num_of_participants+1];
+      for(int i=0; i<current_num_of_participants; i++){
+        array[i] = participants[i];
+      }
+      array[current_num_of_participants+1] = buddy;
+      participants = array;
+    }
+    return true;
+  }
   return false;
 }
 bool WorkoutEnvironment::start_workout(int participant_index, int workout_index) const{
+  if(participant_index >= 0
+    && participant_index <= current_num_of_participants
+    && workout_index >= 0
+    && workout_index <= current_num_of_workouts
+    && participants[participant_index]->get_energy() >= available_workouts[workout_index].get_energy_change()){
+      participants[participant_index]->set_fat(participants[participant_index]->get_fat() + available_workouts[workout_index].get_fat_change());
+      participants[participant_index]->set_muscle(participants[participant_index]->get_muscle() + available_workouts[workout_index].get_muscle_change());
+      participants[participant_index]->set_energy(participants[participant_index]->get_energy() + available_workouts[workout_index].get_energy_change());
+      return true;
+  }
   return false;
 }
